79127cbc785cd5a0fe225f1033cb47328ab9ffc8
Josephus Paye II <j.paye96@gmail.com>
2020-02-18 23:03:03 +1100

    Move parser to lib/

--- lib/test/parser.test.ts
+++ lib/test/parser.test.ts
@@ -10,7 +10,7 @@ function parse(input: string) {
 }
 
 describe('parser', () => {
-  it('parser has the expected API', () => {
+  it('has the expected API', () => {
     expect(typeof parser.parse).toBe('function');
     expect(typeof parser.SyntaxError).toBe('function');
   });
@@ -41,5 +41,11 @@ appearance { # Defines styles for how the graph should appear
     const output = `{"nodes":[{"type":"normal","id":null,"label":"Yes it's good","body":"Yep"},{"type":"normal","id":"myNode","label":"Yes \\"sure\\" it is","body":null},{"type":"normal","id":"myOtherNode","label":"Great","body":"\\n  You can write entire poems in here:\\n  ---\\n  Once upon a midnight dreary, while I pondered, weak and weary...\\n"},{"type":"branch","id":null,"label":"Do the thing?","body":null,"cases":[{"label":"Yes","nodes":[{"type":"normal","id":null,"label":"It's a yes","body":null}]},{"label":"No","nodes":[{"type":"normal","id":null,"label":"It's a no","body":null}]}]}],"appearance":[{"nodeId":"myNode","modifiers":[{"shape":"rectangle"},{"style":{"id":"1","options":[{"type":"enable","option":"fill"},{"type":"disable","option":"border"}]}}]}]}`;
 
     expect(JSON.stringify(parse(input))).toBe(output);
+  });
+
+  it('throws on syntax errors', () => {
+    expect(() => {
+      parse(`node `);
+    }).toThrow(Error);
   });
 });
