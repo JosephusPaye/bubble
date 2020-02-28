3f6814ec0de7b7090acc6ea1367c0531f5a665fc
Josephus Paye II <j.paye96@gmail.com>
2020-02-29 00:54:36 +1100

    Temporarily disable purgecss for monaco-editor

--- editor/postcss.config.js
+++ editor/postcss.config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    // ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    // TODO: Fix purgecss for monaco-editor styles and enable
   ],
 };
